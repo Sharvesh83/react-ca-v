75d60de84e7d17f149fd3c0c5383229ceb804c6e
Sharvesh83 <sharvesh.somasundaram@kalvium.community>
2023-08-20 16:08:12 +0530

    some changes

--- src/components/RegistrationForm.jsx
+++ src/components/RegistrationForm.jsx
@@ -9,13 +9,6 @@ function RegistrationForm() {
     repeatPassword: ''
   });
 
-  const [errors, setErrors] = useState({
-    name: '',
-    email: '',
-    password: '',
-    repeatPassword: ''
-  });
-
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setFormData({ ...formData, [name]: value });
@@ -29,56 +22,43 @@ function RegistrationForm() {
     const passwordError = validatePassword(formData.password);
     const repeatPasswordError = validateRepeatPassword(formData.password, formData.repeatPassword);
 
-    if (nameError || emailError || passwordError || repeatPasswordError) {
-      setErrors({
-        name: nameError,
-        email: emailError,
-        password: passwordError,
-        repeatPassword: repeatPasswordError
-      });
-    } else {
-      setErrors({
+    if (!nameError && !emailError && !passwordError && !repeatPasswordError) {
+      alert('Registered successfully!');
+      setFormData({
         name: '',
         email: '',
         password: '',
         repeatPassword: ''
       });
-      alert('Registered successfully!');
     }
   };
 
   const validateName = (name) => {
-    if (name.length < 3) {
-      return 'Name should be at least 3 characters long.';
+    if (name.length < 3 || name.length > 30) {
+      return true;
     }
-    if (name.length > 30) {
-      return 'Name should not be greater than 30 characters.';
-    }
-    return '';
+    return false;
   };
 
   const validateEmail = (email) => {
     if (!email.includes('@')) {
-      return 'Please enter a valid email address.';
+      return true;
     }
-    return '';
+    return false;
   };
 
   const validatePassword = (password) => {
-    if (password.length < 10) {
-      return 'Password should be at least 10 characters long.';
+    if (password.length < 10 || !/[!@#$%^&*()_+{}[\]:;<>,.?~=-]/.test(password)) {
+      return true;
     }
-    if (!/[!@#$%^&*()_+{}[\]:;<>,.?~=-]/.test(password)) {
-      return 'Password should contain at least one special character.';
-    }
-    return '';
+    return false;
   };
 
   const validateRepeatPassword = (password, repeatPassword) => {
     if (password !== repeatPassword) {
-      return 'Passwords do not match.';
+      return true;
     }
-    return '';
+    return false;
   };
 
   return (
@@ -86,18 +66,14 @@ function RegistrationForm() {
       <h2>Register</h2>
       <form onSubmit={handleSubmit}>
         <input type="text" name="name" placeholder="Your Name" value={formData.name} onChange={handleInputChange} />
-        {errors.name && <div className="error">{errors.name}</div>}
         
         <input type="email" name="email" placeholder="Your Email" value={formData.email} onChange={handleInputChange} />
-        {errors.email && <div className="error">{errors.email}</div>}
         
         <input type="password" name="password" placeholder="Password" value={formData.password} onChange={handleInputChange} />
-        {errors.password && <div className="error">{errors.password}</div>}
         
         <input type="password" name="repeatPassword" placeholder="Repeat Password" value={formData.repeatPassword} onChange={handleInputChange} />
-        {errors.repeatPassword && <div className="error">{errors.repeatPassword}</div>}
         
-        <button type="submit" disabled={Object.values(errors).some(error => error !== '')}>Register</button>
+        <button type="submit">Register</button>
       </form>
     </div>
   );
